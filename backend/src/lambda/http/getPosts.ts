55edd6016ac071226cd44be87a00115049adf21d
abdennour001 <amokranabdennour@gmail.com>
2020-09-23 17:13:05 +0100

    fix update

--- backend/src/lambda/http/getPosts.ts
+++ backend/src/lambda/http/getPosts.ts
@@ -41,7 +41,8 @@ export const handler: APIGatewayProxyHandler = async (
   return {
     statusCode: 200,
     headers: {
-      'Access-Control-Allow-Origin': '*'
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true
     },
     body: JSON.stringify({ items: result })
   }
